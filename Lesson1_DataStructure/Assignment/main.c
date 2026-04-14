c47d3f9699ac3e4c60a490940b02ba126c47413e
MahmudElyamani <mahmoud.elyamani.7@gmail.com>
2026-04-14 14:25:31 +0200

    Finished assignment 1 except for last function.

--- Lesson1_DataStructure/Assignment/main.c
+++ Lesson1_DataStructure/Assignment/main.c
@@ -97,6 +97,9 @@ int DeleteStudent()
                 {
                     gpFirstStudent = pSelectedStudent->pNextStudent;
                 }
+                printf("\nStudent deleted:\n");
+                printf("Name: %s\n",pSelectedStudent->student.name);
+                printf("ID: %d\n",pSelectedStudent->student.ID);
                 free (pSelectedStudent);
                 return 1;
             }
@@ -152,15 +155,27 @@ void DeleteAll()
     }  
 }
 
-void getNth()
+int getNth()
 {
     char tempText[10];
-    int indexNumber, count = 1;
+    int solNum, indexNumber, count = 1, count2= 1;
     struct SStudent *pTempStudent = gpFirstStudent;
+    struct SStudent *mainPtr      = gpFirstStudent;
+    struct SStudent *refPtr       = gpFirstStudent;
     if(!pTempStudent)
-    printf("\nThere's no students on this list.\n");
-    else
     {
+        printf("\nThere are no students on this list.\n");
+        return 0;
+    }
+    printf("\nPlease pick a solution method:");
+    printf("\n1. From start ");
+    printf("\n2. From End ");
+    printf("\n3. Exit\n");
+    gets(tempText);
+    solNum = atoi(tempText);
+    switch (solNum)
+    {
+        case 1:
         printf("\nEnter an index number: ");
         gets(tempText);
         indexNumber = atoi(tempText);
@@ -174,10 +189,151 @@ void getNth()
             else
             {
                 printf("The index number(%d) exceeds the number of nodes(%d)!\n",indexNumber,count);
-                break;
+                return 0;
             }
         }
         printf("The index number %x has node ID of: %d",indexNumber,pTempStudent->student.ID);
+        break;
+        case 2:
+        printf("\nEnter an index number: ");
+        gets(tempText);
+        indexNumber = atoi(tempText);
+        while (refPtr)
+        {
+            refPtr = refPtr->pNextStudent;
+            count++;
+        }
+        if ((count - indexNumber) < 0)
+        {
+            printf("The index number(%d) exceeds the number of nodes(%d)!\n",indexNumber,count);
+            return 0;
+        }
+        while (count2 != (count-indexNumber))
+        {
+            mainPtr = mainPtr->pNextStudent;
+            count2++;
+        }
+        printf("The index number %x has node ID of: %d",indexNumber,mainPtr->student.ID);
+        break;
+        case 3:
+        break;
+        default:
+        printf("\nThe number you entered is not valid, try again.");
+        getNth();
+        break;
+    }
+    return 1;
+}
+
+int lengthRecursive(struct SStudent *pStudent)
+{
+    if (pStudent == NULL)
+    return 0;
+    else
+    {
+        return 1 + lengthRecursive(pStudent->pNextStudent);
+    }
+}
+
+int findLength()
+{
+    char tempText[10];
+    int solNum, count = 0;
+    struct SStudent *pTempStudent = gpFirstStudent;
+    printf("\nPlease pick a solution method:");
+    printf("\n1. Iterative");
+    printf("\n2. Recursive");
+    printf("\n3. Exit\n");
+    gets(tempText);
+    solNum = atoi(tempText);
+    if(pTempStudent == NULL)
+    {
+        printf("\nLength = 0");
+        return 0;
+    }
+    switch (solNum)
+    {
+        case 1:
+        while (pTempStudent)
+        {
+            pTempStudent = pTempStudent->pNextStudent;
+            count++;
+        }
+        printf("\nLength = %d using iteritve mehod.\n",count);
+        break;
+        case 2:
+        count = lengthRecursive(pTempStudent);
+        printf("\nLength = %d using recursive mehod.\n",count);
+        break;
+        case 3:
+        break;
+        default:
+        printf("\nThe number you entered is not valid, try again.");
+        findLength();
+        break;
+    }
+    return 1;
+}
+
+void findMiddle()
+{
+    struct SStudent *refPtr     = gpFirstStudent;
+    struct SStudent *mainPtr   = gpFirstStudent;
+    int count=1, count2=1;
+    char condition = 1;
+    if  (!gpFirstStudent)
+    printf("\nThere's no students on this list.");
+    else
+    {
+        while (refPtr->pNextStudent)
+        {
+            refPtr = refPtr->pNextStudent;
+            count++;
+        }
+        while (count2 <= (count/2))
+        {
+            if (count < 3)
+            {
+                printf("\nThere's no sufficient amount of nodes to find the middle.\n");
+                condition = 0;
+                break;
+            }
+            mainPtr = mainPtr->pNextStudent;
+            count2++;
+        }
+        if (condition)
+        {
+            printf("\n The middle of the list is node no.%d",count2);
+            printf("\n ID: %d",mainPtr->student.ID);
+            printf("\n Name: %s",mainPtr->student.name);
+            printf("\n Height: %.2f",mainPtr->student.height);
+        }
+    }
+}
+
+void reverseList()
+{
+    struct SStudent *pPreviousStudent   = NULL;
+    struct SStudent *pCurrentStudent    = gpFirstStudent;
+    struct SStudent *pNextAddress       = gpFirstStudent;
+    struct SStudent *pHolder            = gpFirstStudent;
+    if (!gpFirstStudent)
+    printf("\nThere are no students on this list.");
+    else
+    {
+        while (pNextAddress)
+        {
+            //moved to the next element of old list
+            pNextAddress = pNextAddress->pNextStudent;
+            //redirecting the element to reverse direction  
+            pCurrentStudent->pNextStudent = pPreviousStudent;
+            //moving our location mover to a new location
+            pPreviousStudent = pCurrentStudent;
+            //moving our location reference to a new location
+            pCurrentStudent = pNextAddress;
+        }
+        pNextAddress = pHolder;
+        printf("\nReversing the list is now complete, please select 3 to check.");
     }
 }
 
@@ -193,6 +349,9 @@ int main()
         printf("\n   3: View Students ");
         printf("\n   4: Delete All Students ");
         printf("\n   5: get node ID through index number ");
+        printf("\n   6: Calculate length of list ");
+        printf("\n   7: Find the middle of the list ");        
+        printf("\n   8: Reverse list (Not Working)");        
         printf("\n -Enter option number: ");
 
         gets(tempText);
@@ -214,11 +373,19 @@ int main()
             case 5:
             getNth();
             break;
+            case 6:
+            findLength();
+            break;
+            case 7:
+            findMiddle();
+            break;
+            case 8:
+            reverseList();
+            break;
             default:
             printf("\n Please input a valid number!");
             break;
         }
     }
-
     return 0;
 }
